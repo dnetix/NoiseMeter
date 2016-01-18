50de1c83503df05be4fb0650b55a28e4b8891269
Diego Calle <dnetix@gmail.com>
2016-01-18 18:43:07 -0500

    Application working!

--- app/src/main/java/udea/edu/co/noisemeter/MainActivity.java
+++ app/src/main/java/udea/edu/co/noisemeter/MainActivity.java
@@ -1,13 +1,270 @@
 package udea.edu.co.noisemeter;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtLevel;
+    private TextView txtBreakCount;
+    private ProgressBar progressBar;
+
+    SharedPreferences preferences;
+    private double minLevel;
+    private double maxLevel;
+
+    boolean isBound = false;
+    Messenger mService = null;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    // Context Menu Constants
+    static int PREFERENCES_GROUP_ID = 0;
+    static final int PREF_CALIBRATE = 5;
+    static final int PREF_START_SERVICE = 6;
+    static final int PREF_KILL_SERVICE = 7;
+    static final int PREF_RESTORE_DEFAULT = 8;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = new Messenger(service);
+            try {
+                Message msg = Message.obtain(null, SoundService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            }catch (Exception e){
+                Log.v("SOUND", e.getMessage());
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+    };
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SoundService.MSG_DB_UPDATE:
+                    Double amp = msg.getData().getDouble("DB");
+                    txtLevel.setText(getLevelOfNoise(amp));
+                    txtBreakCount.setText(String.valueOf(msg.arg1));
+                    updateProgressFromDB(amp);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLevel = (TextView) findViewById(R.id.txtLevel);
+        txtBreakCount = (TextView) findViewById(R.id.txtBreakCount);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        progressBar.setMax(100);
+
+        if(!SoundService.isRunning()){
+            doStartService();
+        }
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        minLevel = Double.valueOf(preferences.getString("minLevel", "-40"));
+        maxLevel = Double.valueOf(preferences.getString("maxLevel", "-20"));
+
+        findViewById(R.id.calibrationLayout).setVisibility(View.INVISIBLE);
     }
+
+    public void updateProgressFromDB(Double amp){
+        int progress = (int) Math.round(((minLevel - amp) / (minLevel - maxLevel)) * 100);
+        if(progress > 100){
+            progressBar.setProgress(100);
+            progressBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        }else{
+            progressBar.setProgress(progress);
+            if(progress > 70){
+                progressBar.getProgressDrawable().setColorFilter(Color.rgb(255, 140, 0), PorterDuff.Mode.SRC_IN);
+            }else{
+                progressBar.getProgressDrawable().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        doBindService();
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        doUnbindService();
+        super.onStop();
+    }
+
+    public void startAlerting(View view){
+        doStartAlerting();
+    }
+
+    public void doStartAlerting(){
+        try {
+            Message msg = Message.obtain(null, SoundService.MSG_START_ALERTING);
+            mService.send(msg);
+        }catch (Exception e){
+            Log.v("SOUND", e.getMessage());
+        }
+    }
+
+    public void stopAlerting(View view){
+        doStopAlerting();
+    }
+
+    private void doStopAlerting(){
+        try {
+            Message msg = Message.obtain(null, SoundService.MSG_STOP_ALERTING);
+            mService.send(msg);
+        }catch (Exception e){
+            Log.v("SOUND", e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(PREFERENCES_GROUP_ID, PREF_CALIBRATE, 0, "Calibrar").setIcon(android.R.drawable.ic_input_add);
+        menu.add(PREFERENCES_GROUP_ID, PREF_START_SERVICE, 0, "Iniciar Servicio").setIcon(android.R.drawable.ic_input_add);
+        menu.add(PREFERENCES_GROUP_ID, PREF_KILL_SERVICE, 0, "Detener Servicio").setIcon(android.R.drawable.ic_delete);
+        menu.add(PREFERENCES_GROUP_ID, PREF_RESTORE_DEFAULT, 0, "Restaurar Configuracion").setIcon(android.R.drawable.ic_delete);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case PREF_CALIBRATE:
+                doCalibrateOptions();
+                break;
+            case PREF_START_SERVICE:
+                doStartService();
+                break;
+            case PREF_KILL_SERVICE:
+                doStopService();
+                break;
+            case PREF_RESTORE_DEFAULT:
+                preferences.edit().clear().apply();
+                maxLevel = Double.valueOf(preferences.getString("maxLevel", "-20"));
+                updateTolerance(0);
+                break;
+        }
+        return true;
+    }
+
+    private void doCalibrateOptions() {
+        doStopAlerting();
+        findViewById(R.id.calibrationLayout).setVisibility(View.VISIBLE);
+    }
+
+    public void endCalibrateOptions(View view){
+        doEndCalibrateOptions();
+    }
+
+    private void doEndCalibrateOptions(){
+        findViewById(R.id.calibrationLayout).setVisibility(View.INVISIBLE);
+        doStartAlerting();
+    }
+
+    public void lessTolerance(View view){
+        updateTolerance(-1);
+    }
+
+    public void moreTolerance(View view){
+        updateTolerance(+1);
+    }
+
+    public void updateTolerance(int change){
+        maxLevel += change;
+        preferences.edit().putString("maxLevel", String.valueOf(maxLevel)).apply();
+        try {
+            Message msg = Message.obtain(null, SoundService.MSG_UPDATE_THRESHOLD);
+            mService.send(msg);
+        }catch (Exception e){
+            Log.v("SOUND", e.getMessage());
+        }
+    }
+
+    public void postMessage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public void doStartService() {
+        if(!SoundService.isRunning()) {
+            startService(new Intent(this, SoundService.class));
+            doBindService();
+        }
+    }
+
+    public void doStopService() {
+        if(SoundService.isRunning()) {
+            doUnbindService();
+            stopService(new Intent(this, SoundService.class));
+        }
+    }
+
+    public void doBindService(){
+        if(!isBound){
+            bindService(new Intent(this, SoundService.class), mConnection, Context.BIND_AUTO_CREATE);
+            isBound = true;
+        }
+    }
+
+    public void doUnbindService(){
+        if(isBound){
+            unbindService(mConnection);
+            isBound = false;
+        }
+    }
+
+    public String getLevelOfNoise(Double amp){
+        return "Nivel de Ruido: " + String.valueOf(round((minLevel * -1) + amp, 2));
+    }
+
+    /**
+     * Utility function for rounding decimal values
+     */
+    public double round(double d, int decimalPlace) {
+        // see the Javadoc about why we use a String in the constructor
+        // http://java.sun.com/j2se/1.5.0/docs/api/java/math/BigDecimal.html#BigDecimal(double)
+        BigDecimal bd = new BigDecimal(Double.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.doubleValue();
+    }
+
 }
---
+++ app/src/main/java/udea/edu/co/noisemeter/SoundService.java
@@ -0,0 +1,197 @@
+package udea.edu.co.noisemeter;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class SoundService extends IntentService {
+
+    public static final int MSG_REGISTER_CLIENT = 1;
+    public static final int MSG_UNREGISTER_CLIENT = 2;
+    public static final int MSG_DB_UPDATE = 3;
+    public static final int MSG_KILL_SERVICE = 4;
+    public static final int MSG_START_ALERTING = 5;
+    public static final int MSG_STOP_ALERTING = 6;
+    public static final int MSG_UPDATE_THRESHOLD = 7;
+
+    private static boolean isRunning = false;
+
+    Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    private MediaRecorder mediaRecorder = null;
+    private boolean listening = true;
+    private boolean playing = false;
+    private boolean alerting = false;
+    private double maxLevel;
+    private int maxBreaks = 3;
+    private int breakCount = 0;
+    private long lastBreak = -1;
+    private long minTimeInMils = 3000;
+    private int delay = 400;
+    Messenger mClient;
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MSG_REGISTER_CLIENT:
+                    mClient = msg.replyTo;
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    mClient = null;
+                    break;
+                case MSG_KILL_SERVICE:
+                    stop();
+                    break;
+                case MSG_START_ALERTING:
+                    alerting = true;
+                    break;
+                case MSG_STOP_ALERTING:
+                    alerting = false;
+                    break;
+                case MSG_UPDATE_THRESHOLD:
+                    updatePreferences();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    public SoundService() {
+        super("sound-service");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(mediaRecorder == null){
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile("/dev/null");
+
+            try {
+                mediaRecorder.prepare();
+            } catch (Exception e) {
+                Log.e("error", e.getMessage());
+            }
+
+            mediaRecorder.start();
+        }
+
+        updatePreferences();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    private void updatePreferences() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        maxLevel = Double.valueOf(preferences.getString("maxLevel", "-20"));
+    }
+
+    @Override
+    public void onDestroy() {
+        this.listening = false;
+        if(mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+        isRunning = false;
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        isRunning = true;
+        while (listening){
+            SystemClock.sleep(delay);
+            Double amp = 20 * Math.log10(getAmplitude() / 32768.0);
+
+            if(!Double.isInfinite(amp)){
+                if (amp > maxLevel && alerting) {
+                    if (playing) {
+                        playing = false;
+                    } else {
+                        long time = System.currentTimeMillis();
+                        if (lastBreak != -1) {
+                            if ((time - lastBreak) < minTimeInMils) {
+                                breakCount++;
+                                if (breakCount >= maxBreaks) {
+                                    makeAlert();
+                                    lastBreak = -1;
+                                    breakCount = 0;
+                                }
+                            } else {
+                                lastBreak = -1;
+                                breakCount = 0;
+                            }
+                        } else {
+                            lastBreak = time;
+                        }
+                    }
+                }
+                reportData(amp);
+            }
+        }
+        isRunning = false;
+    }
+
+    private void reportData(Double amp) {
+        if(mClient != null) {
+            try {
+                Message msg = Message.obtain(null, MSG_DB_UPDATE, breakCount, 0);
+                Bundle b = new Bundle();
+                b.putDouble("DB", amp);
+                msg.setData(b);
+                mClient.send(msg);
+            } catch (Exception e) {
+                mClient = null;
+            }
+        }
+    }
+
+    public void makeAlert(){
+        playing = true;
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.alarm);
+        mp.start();
+    }
+
+    private double getAmplitude() {
+        if(mediaRecorder != null){
+            return mediaRecorder.getMaxAmplitude();
+        }else{
+            return 0.0;
+        }
+    }
+
+    public void stop(){
+        this.listening = false;
+    }
+
+    public static boolean isRunning(){
+        return isRunning;
+    }
+
+}
